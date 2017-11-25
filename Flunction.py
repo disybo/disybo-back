163dcc1acc001c44375b182fe227384df83d48c9
Johan Mickos <johan.mickos@gmail.com>
2017-11-25 01:32:40 +0100

    Implement end-to-end data flow from DB -> lookup -> frontend

--- Flunction.py
+++ Flunction.py
@@ -1,15 +1,31 @@
 from flask import Flask
 from flask_cors import CORS
 
+from config import DevelopmentConfig
+from database import db
+from models.boy import Boy
 
 app = Flask(__name__)
 CORS(app)
 
+app.config.from_object(DevelopmentConfig)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello Junction 2017! Can I get a hooray?! Daaaaaaaaang.\n'
+def blank():
+    return 'Hello, world'
+
+
+@app.route('/<int:user_id>')
+def hello_world(user_id):
+    try:
+        name = Boy.query.get(user_id).name
+        return "<h1>Hello, {}</ht>".format(name)
+    except Exception as ex:
+        print(ex)
+        return '<h1>Something is broken.</h1>'
 
 
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(debug=True)
