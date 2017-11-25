5a8d23dc9312fa0a1ebc326b4ea9d339055ff710
Johan Mickos <johan.mickos@gmail.com>
2017-11-25 09:42:02 +0100

    Impl. sample API for querying users, tables

--- Flunction.py
+++ Flunction.py
@@ -17,8 +17,23 @@ def blank():
     return 'Hello, world'
 
 
-@app.route('/<int:user_id>')
-def hello_world(user_id):
+@app.route('/tables')
+def tables():
+    from sqlalchemy import MetaData
+    metadata = MetaData()
+    metadata.reflect(bind=db.engine)
+    tables = metadata.tables.keys()
+    return " ".join(tables)
+
+
+@app.route('/users')
+def all_users():
+    users = db.session.query(Boy).all()
+    return u"<br>".join([u"{0}: {1}".format(user.name, user.role) for user in users])
+
+
+@app.route('/user/<int:user_id>')
+def user(user_id):
     try:
         name = Boy.query.get(user_id).name
         return "<h1>Hello, {}</ht>".format(name)
