5774f3ee2a600a0bec486771e25543004c14d329
VilhelmF <vfridriks@simnet.is>
2017-11-25 12:56:55 +0100

    Started working on a API call for Sindri to gather all refueling events sorted by station names. Need the data though in order to proceed.

--- api/vehicle_data/vehicles.py
+++ api/vehicle_data/vehicles.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from .models import Boy
+from .models import Boy, Vehicle, VehicleType, RefuelEvent, FuelStation, FuelType
 from database import db
 
 vehicles = Blueprint('vehicles', 'vehicles', url_prefix='/api/vehicles')
@@ -13,3 +13,15 @@ def hello_world(user_id):
     except Exception as ex:
         print(ex)
         return '<h1>Something is broken.</h1>'
+
+
+@vehicles.route('/stations/fuel')
+def fuel_data():
+    refueling_data = {}
+    fuel_stations = FuelStation.query.all()
+    for fuel_station in fuel_stations:
+        name = fuel_station.display_name
+        refuel_events = RefuelEvent.query.filter(station_id=fuel_station.id)
+    return 'Ok'
+
+
