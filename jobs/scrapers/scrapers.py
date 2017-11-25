7d9e99b5dad7e6dcca16a29f0711fc9f0b86dcc5
Johan Mickos <johan.mickos@gmail.com>
2017-11-25 14:42:26 +0100

    Update vehicle scraper to extract/bootstrap all relevant data

---
+++ jobs/scrapers/scrapers.py
@@ -0,0 +1,52 @@
+import requests
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from common.globals import STARA_API_BASE
+
+
+class DBConnector(object):
+    engine = None
+    session = None
+
+    def __init__(self, uri):
+        self.engine = create_engine(uri)
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session(autoflush=True)
+
+
+""" Scrapes an API and returns the JSON data """
+
+
+class StaraAPIScraper(object):
+    app_key = ""
+    base_url = ""
+    headers = {}
+
+    def __init__(self, base_url=STARA_API_BASE, app_key='f848dba8-adc9-45e5-9771-b51b0ffa700a'):
+        self.base_url = base_url
+        self.app_key = app_key
+        self.headers = {
+            'appKey': self.app_key,
+            'x-thingworx-session': 'true',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+
+    def get(self, resource, payload=None):
+        if payload is None:
+            payload = {}
+        r = requests.get(url="{}/{}".format(self.base_url, resource), headers=self.headers, params=payload)
+        return r
+
+    def post(self, resource, payload=None):
+        if payload is None:
+            payload = {}
+        r = requests.post(url="{}/{}".format(self.base_url, resource), headers=self.headers, params=payload)
+        return r
+
+
+if __name__ == '__main__':
+    scraper = VehicleScraper()
+    scraper.bootstrap()
+    scraper.get_all_vehicles()
