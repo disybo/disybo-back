38cfe4551aab77bfe16852eae92bcf7f5b4ffa7d
Johan Mickos <johan.mickos@gmail.com>
2017-11-25 11:11:39 +0100

    Fix globals structure

--- common/globals.py
+++ common/globals.py
@@ -1,12 +1,12 @@
 """ Global configurations and static data"""
 
 # Stara API and endpoints (https://documenter.getpostman.com/view/1976410/stara/6tc44mC#intro)
-StaraAPI = {}
-StaraAPI.endpoints = {}
-StaraAPI.base = "https://dev-stara.elisaiot.com/Thingworx/Things/Stara/Services/"
-StaraAPI.endpoints.vehicles = "{}/Vehicles".format(StaraAPI.base)
-StaraAPI.endpoints.refuel_events = "{}/Refuel".format(StaraAPI.base)
-StaraAPI.endpoints.vehicle_loc = "{}/Location".format(StaraAPI.base)
-StaraAPI.endpoints.garage_info = "{}/Garage".format(StaraAPI.base)
-StaraAPI.endpoints.garage_by_vehicle = "{}/GarageByVehicle".format(StaraAPI.base)
-StaraAPI.endpoints.garage_by_sehiid = "{}/GarageBySEHIID".format(StaraAPI.base)
+STARA_API_BASE = "https://dev-stara.elisaiot.com/Thingworx/Things/Stara/Services"
+STARA_ENDPOINTS = {
+        'vehicles': "{}/Vehicles".format(STARA_API_BASE),
+        'refuel_events': "{}/Refuel".format(STARA_API_BASE),
+        'vehicle_loc': "{}/Location".format(STARA_API_BASE),
+        'garage_info': "{}/Garage".format(STARA_API_BASE),
+        'garage_by_vehicle': "{}/GarageByVehicle".format(STARA_API_BASE),
+        'garage_by_sehiid': "{}/GarageBySEHIID".format(STARA_API_BASE)
+}
