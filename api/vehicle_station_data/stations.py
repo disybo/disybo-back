5d25fea0f351e818d66960ad45913afbd6d1f54a
Sindri Már Kaldal Sigurjónsson <sindrimars@gmail.com>
2017-11-26 00:50:54 +0200

    added a function to get the types

--- api/vehicle_station_data/stations.py
+++ api/vehicle_station_data/stations.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, Response, jsonify
-from api.vehicle_station_data.models import FuelStation, RefuelEvent, Boy
+from api.vehicle_station_data.models import FuelStation, RefuelEvent, Boy, FuelType
 from datetime import datetime
 from sqlalchemy.sql import func
 from database import db
@@ -40,6 +40,36 @@ def get_overall_consumption():
             {'station_id': fs.station_id,
              'display_name': fs.display_name,
              'fuel_volume': refuel_sum}
+        )
+    return Response(json.dumps(json_list), mimetype='application/json')
+
+
+@stations.route('/fuel/type')
+def get_fuel_type():
+    request_start_date = request.args.get('start')
+    request_end_date = request.args.get('end')
+    # granularity = request.args.get('granularity')
+
+    start_date = datetime.strptime(request_start_date.split('T')[0], '%Y-%m-%d')
+    end_date = datetime.strptime(request_end_date.split('T')[0], '%Y-%m-%d')
+
+    json_list = []
+    fuel_stations = FuelStation.query.all()
+    fuel_types = FuelType.query.all()
+
+    for fs in fuel_stations:
+        fuel_sums = []
+        for fuel in fuel_types:
+            fuel_sum = RefuelEvent.query.with_entities(func.sum(RefuelEvent.fuel_volume).label('sum')).filter(
+                RefuelEvent.fuel_type == fuel.id,
+                RefuelEvent.station_id == fs.station_id,
+                RefuelEvent.time.between(start_date, end_date)
+            ).scalar()
+            fuel_sums.append({"fuel_name": fuel.display_name, "amount": fuel_sum})
+        json_list.append(
+            {'station_id': fs.station_id,
+             'display_name': fs.display_name,
+             'fuel_volume': fuel_sums}
         )
     return Response(json.dumps(json_list), mimetype='application/json')
 
