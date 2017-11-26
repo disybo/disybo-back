ed69dc70d0e52bf0ccd7b99ada100bcdf3cd4b35
VilhelmF <vfridriks@simnet.is>
2017-11-26 03:09:46 +0100

    Removed a dummy function from vehicles.py

--- api/vehicle_station_data/vehicles.py
+++ api/vehicle_station_data/vehicles.py
@@ -15,16 +15,6 @@ from api import Notification
 vehicles = Blueprint('vehicles', 'vehicles', url_prefix='/api/vehicles')
 
 
-@vehicles.route('/boys/<int:user_id>')
-def hello_world(user_id):
-    try:
-        name = Boy.query.get(user_id).name
-        return "<h1>Hello, {}</ht>".format(name)
-    except Exception as ex:
-        print(ex)
-        return '<h1>Something is broken.</h1>'
-
-
 @vehicles.route('/types')
 def get_car_types():
     vehicle_types = VehicleType.query.all()
